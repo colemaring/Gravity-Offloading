db9f5b07f7ea84cd767b14483a73fa8a3e7de9fb
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-11-07 11:28:53 -0500

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Loadcell from "./components/Loadcell";
+
+function App() {
+  return (
+    <>
+      <Loadcell />
+    </>
+  );
+}
+
+export default App;
