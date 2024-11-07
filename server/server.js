54e3032462a9cb9d418e668913132767a5e58114
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-11-07 11:30:23 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const express = require('express');
+const path = require('path');
+
+// Create an Express app
+const app = express();
+
+// Serve static files from the 'dist' folder
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Start HTTP server on port 3000
+const server = app.listen(3000, () => {
+    console.log('HTTP server running on http://localhost:3000');
+});
+
+// Create a WebSocket server on top of the HTTP server
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
     console.log('Client connected');
 
+    // When a message is received from the C++ client
     ws.on('message', message => {
         console.log('Received mass data: ' + message);
 
-
+        // Send the message to all connected clients
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
     });
 
     ws.on('close', () => {
