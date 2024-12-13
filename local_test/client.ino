5053f88da95261b5902749fad3fb0c2b630895c0
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-12-13 11:50:18 -0500

    Update client.ino

--- local_test/client.ino
+++ local_test/client.ino
@@ -1,12 +1,15 @@
 #include "MPU9250.h"
+#include <HX711.h>
 
 MPU9250 mpu;
+HX711 scale;
+long rawValue;
 
 void setup() {
     Serial.begin(115200);
     Wire.begin(18, 19);
+    scale.begin(17, 5); // data, clock
     delay(2000);
-
     mpu.setup(0x68);
 }
 
@@ -14,6 +17,11 @@ void loop() {
     if (mpu.update()) {
         Serial.print(mpu.getYaw()); Serial.print(", ");
         Serial.print(mpu.getPitch()); Serial.print(", ");
-        Serial.println(mpu.getRoll());
+        Serial.print(mpu.getRoll()); Serial.print(", ");
+        Serial.println(rawValue);
+    }
+    if (scale.is_ready())
+    {
+      rawValue = scale.read();
     }
 }
